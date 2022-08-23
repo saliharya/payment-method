d386f7e1e3cac14a79025bc1e0b0f6903e242b33
Salih Arya Gumilang <saliharyazinc@gmail.com>
2022-08-23 14:15:32 +0700

    Create choose payment page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,15 @@
 import type { NextPage } from 'next';
+import Link from 'next/link';
 
 const Home: NextPage = () => {
 	return (
-		<>
-			<button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+		<> <div className="flex items-center justify-center h-screen">
+			<Link href='/choose-payment'>
+				<button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
         Choose Payment
-			</button>
+				</button>
+			</Link>
+		</div>
 		</>
 	);
 };
