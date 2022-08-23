a69977d6c26eefc37753b4eb1dee6d0359fe5ecd
Salih Arya Gumilang <saliharyazinc@gmail.com>
2022-08-23 17:07:29 +0700

    display categories

--- components/molecules/PaymentMethod.tsx
+++ components/molecules/PaymentMethod.tsx
@@ -1,26 +1,32 @@
 import Image from 'next/image';
 
 interface itemProps {
-	name? : string
-    image : string;
-    balance? : number;
-    point? : number
+	category : string
+	name?: string
+	image?: string;
+	balance?: number;
+	point?: number
 }
 
-export default function PaymentMethod(props:itemProps) {
+export default function PaymentMethodItems(props: itemProps) {
 
-	const {name, image, balance, point} = props;
+	const { category, name, image, balance, point } = props;
 
 	return (
-		<div className="flex items-center">
-			<div className="p-3">
-				<Image src={image} width={40} height={40}></Image>
-			</div>
+		<>
 			<div className="font-bold">
-				<p>{balance}</p>
-				<p>{point}</p>
-				<p>{name}</p>
+				<p>{category}</p>
 			</div>
-		</div>
+			<div className="flex items-center">
+				<div className="p-3">
+					<Image src={image} width={40} height={40}></Image>
+				</div>
+				<div className="font-medium">
+					<p>{balance}</p>
+					<p>{point}</p>
+					<p>{name}</p>
+				</div>
+			</div>
+		</>
 	);
 }
--- pages/choose-payment/index.tsx
+++ pages/choose-payment/index.tsx
@@ -3,58 +3,64 @@ import PaymentMethod from '../../components/molecules/PaymentMethod';
 const paymentDatas = [
 	{
 		id: 0,
-		logo: '/pngwing.com.png',
-		balance: 0,
-		point: 0
+		categoryName:'Pembayaran via Tokopedia',
+		items: [
+			{
+				logo: '/pngwing.com.png',
+				balance: 0,
+				point: 0
+			},
+			{
+				logo: '/pngwing.com.png',
+				balance: 112675,
+				point: 5000
+			},
+		]
 	},
 	{
 		id: 1,
-		logo: '/pngwing.com.png',
-		balance: 112675,
-		point: 5000
+		categoryName: 'Debit Instan',
+		items: [
+			{
+				logo: '/pngwing.com.png',
+				title: 'Direct Debit BRI'
+			},
+			{
+				logo: '/pngwing.com.png',
+				title: 'OneKlik'
+			},
+			{
+				logo: '/pngwing.com.png',
+				title: 'OCTO CIMB Niaga'
+			},
+			{
+				logo: '/pngwing.com.png',
+				title: 'Debit Mandiri'
+			},
+		]
 	},
 	{
 		id: 2,
-		logo: '/pngwing.com.png',
-		name : 'Direct Debit BRI'
-	},
-	{
-		id: 3,
-		logo: '/pngwing.com.png',
-		name: 'OneKlik'
-	},
-	{
-		id: 4,
-		logo: '/pngwing.com.png',
-		name: 'OCTO CIMB Niaga'
-	},
-	{
-		id: 5,
-		logo: '/pngwing.com.png',
-		name: 'Debit Mandiri'
-	},
-	{
-		id: 6,
-		logo: '/pngwing.com.png',
-		name: 'Kartu Kredut / Debit'
+		categoryName: 'Kartu Kredit / Debit',
+		items : [
+			{
+				logo: '/pngwing.com.png',
+				title: 'Kartu Kredit / Debit'
+			},
+		]
 	},
 ];
+
 
 export default function ChoosePayment() {
 	return (
 		<div className="flex items-center justify-center h-screen">
 			<div className="">
-				<h1>Pilih Metode Pembayaran</h1>
-				<h4>Pembayaran di Tokopedia</h4>
-				<div className="flex">
-				</div>
-				<h4>Debit Instan</h4>
 				{
 					paymentDatas.map((paymentData) => (
-						<PaymentMethod key={paymentData.id} image={paymentData.logo} balance={paymentData.balance} point={paymentData.point} />
+						<PaymentMethod key={paymentData.id} category={paymentData.categoryName}/>
 					))
 				}
-				<h4>Kartu Kredit/Debit</h4>
 			</div>
 		</div>
 	);
